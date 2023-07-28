b4c573e594f11ea0dea7a4422f583b0bf7e906b6
qhdudedi <boyoung035@gmail.com>
2023-07-28 22:35:47 +0900

    feat:project 엔티티개발

---
+++ likelion-core/src/main/java/likelion/univ/domain/project/entity/Image.java
@@ -0,0 +1,35 @@
+package likelion.univ.domain.project.entity;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access =  AccessLevel.PROTECTED)
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="image_id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    //원본 파일명
+    private String name;
+
+    // 등록 파일명
+    private String saved;
+
+    public Image(Project project, String name, String saved) {
+        this.project = project;
+        this.name = name;
+        this.saved = saved;
+    }
+}
