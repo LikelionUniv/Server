0e5c09d68dfa72b1bf6b80109d546b54953d4e02
chs98412 <chs98412@gmail.com>
2023-07-28 14:26:42 +0900

    post modeling Post 엔티티 설계, 각 필드의 글자수 제한 논의 필요

--- likelion-core/src/main/java/likelion/univ/domain/entity/Post.java
+++ likelion-core/src/main/java/likelion/univ/domain/entity/Post.java
@@ -1,17 +1,37 @@
 package likelion.univ.domain.entity;
 
 import likelion.univ.common.entity.BaseTimeEntity;
+import likelion.univ.domain.entity.enums.MainCategory;
+import likelion.univ.domain.entity.enums.SubCategory;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @Column(length = 500)
+    private String title;
+
+    @Column(length = 50000)
+    private String body;
+
+    private String thumbnail;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "main_category")
+    private MainCategory mainCategory;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sub_category")
+    private SubCategory subCategory;
+
 }
