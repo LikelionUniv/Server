1340ac0f6d00367117d9d1d3e90554a590c06773
chs98412 <chs98412@gmail.com>
2023-08-03 15:46:55 +0900

    fix: postservice 구현체 수정  코드 복잡한 부분 메소드로 빼내고 예외처리

--- likelion-core/src/main/java/likelion/univ/service/PostService.java
+++ likelion-core/src/main/java/likelion/univ/service/PostService.java
@@ -2,11 +2,12 @@ package likelion.univ.service;
 
 
 import likelion.univ.domain.dto.PostDto;
+import likelion.univ.domain.dto.common.CommonResponseDto;
 
 public interface PostService {
-    public PostDto.Save createPost(PostDto.Save createRequest , Long userId);
+    public CommonResponseDto<Object> createPost(PostDto.Save createRequest , Long userId) ;
 
-    public void updatePost(PostDto.Update updateRequest, Long userId);
+    public CommonResponseDto<Object> updatePost(PostDto.Update updateRequest, Long userId);
 
-    public void deletePost(PostDto.Delete deleteRequest, Long userId);
+    public CommonResponseDto<Object> deletePost(PostDto.Delete deleteRequest, Long userId);
 }
