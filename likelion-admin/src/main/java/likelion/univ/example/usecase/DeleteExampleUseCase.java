10595f40e89c375a7a2323ac3cc0913a457e8689
김슬기 <tmfrk0426@gmail.com>
2023-08-04 18:06:56 +0900

    feat : usecase, adaptor 어노테이션 생성, 및 예시 코드들 추가

--- likelion-admin/src/main/java/likelion/univ/example/usecase/DeleteExampleUseCase.java
+++ likelion-admin/src/main/java/likelion/univ/example/usecase/DeleteExampleUseCase.java
@@ -2,17 +2,18 @@ package likelion.univ.example.usecase;
 
 import likelion.univ.annotation.UseCase;
 import likelion.univ.domain.example.adaptor.ExampleAdaptor;
+import likelion.univ.domain.example.entity.Example;
 import likelion.univ.domain.example.service.ExampleDomainService;
 import likelion.univ.domain.user.adaptor.UserAdaptor;
 import likelion.univ.domain.user.service.UserDomainService;
-import likelion.univ.example.dto.request.CreateExampleRequestDto;
+import likelion.univ.example.dto.response.ExampleInfoResponseDto;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
 @RequiredArgsConstructor
-public class CreateExampleUseCase {
-
+public class DeleteExampleUseCase {
     /* 예시로 적어놓은 유저어댑터 및 도메인서비스입니다. 필요하지않는 싱글톤 인스턴스들은 제거해주세요. */
+
     private final UserAdaptor userAdaptor;
     private final UserDomainService userDomainService;
 
@@ -25,7 +26,9 @@ public class CreateExampleUseCase {
         Transaction을 배제한 도메인에 영향을 주지않는 service라고 생각하시면 쉽습니다.
         Transaction의 영역의 로직은 domainservice 혹의 엔티티에 구현해주세요. (but, 엔티티에 구현은 최대한 자제)(예를들어 유저의경우 changeProfile 같은걸 엔티티에 구현)
     */
-    public void excute(CreateExampleRequestDto createExampleRequestDto){
-        exampleDomainService.createExample(createExampleRequestDto.getBody());
+    public void excute(Long id){
+        Example example = exampleAdaptor.findById(id);
+        exampleAdaptor.delete(example);
     }
+
 }
