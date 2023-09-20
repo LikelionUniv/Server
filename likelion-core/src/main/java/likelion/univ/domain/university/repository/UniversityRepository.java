dadcd4914c0f458a05361e793ff7dd35bf6fe52f
Gahyun-Lee <98022068+Gahyun-Lee@users.noreply.github.com>
2023-09-20 15:59:47 +0900

    Update UniversityRepository.java

--- likelion-core/src/main/java/likelion/univ/domain/university/repository/UniversityRepository.java
+++ likelion-core/src/main/java/likelion/univ/domain/university/repository/UniversityRepository.java
@@ -3,5 +3,8 @@ package likelion.univ.domain.university.repository;
 import likelion.univ.domain.university.entity.University;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UniversityRepository extends JpaRepository<University, Long> {
+import java.util.Optional;
+
+public interface UniversityRepository extends JpaRepository<University,Long> {
+    Optional<University> findByName(String name);
 }
