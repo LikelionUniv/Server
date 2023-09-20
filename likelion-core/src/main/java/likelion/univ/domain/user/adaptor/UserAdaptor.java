36183f21388f588dec7e966c0b9e4dc242dd1971
Gahyun-Lee <98022068+Gahyun-Lee@users.noreply.github.com>
2023-09-20 16:01:45 +0900

    Update UserAdaptor.java

--- likelion-core/src/main/java/likelion/univ/domain/user/adaptor/UserAdaptor.java
+++ likelion-core/src/main/java/likelion/univ/domain/user/adaptor/UserAdaptor.java
@@ -2,18 +2,29 @@ package likelion.univ.domain.user.adaptor;
 
 import likelion.univ.annotation.Adaptor;
 import likelion.univ.domain.user.entity.User;
+import likelion.univ.domain.user.exception.UserNotFoundException;
 import likelion.univ.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 @Adaptor
 @RequiredArgsConstructor
 public class UserAdaptor {
     private final UserRepository userRepository;
 
-    public User findById(Long id) {
-        return userRepository.findById(id).get();
+    public User findById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()-> new UserNotFoundException());
+    }
+
+    public User findByEmail(String email){
+        return userRepository.findByAuthInfoEmail(email)
+                .orElseThrow(()-> new UserNotFoundException());
+    }
+    public Boolean checkEmail(String email){
+        return userRepository.existsByAuthInfoEmail(email);
+    }
+    public User save(User user){
+        return userRepository.save(user);
     }
 
     public List<User> findAll() {
