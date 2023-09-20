1d7d098ee2d04d55f8d09f06acdb9b78be6227a3
wwan13 <wwan13@naver.com>
2023-09-20 20:26:34 +0900

    feat : 컨트롤러에서 받아온 알림 type 에 따라 알맞는 usecase를 반환하는 enum

---
+++ likelion-admin/src/main/java/likelion/univ/alarm/controller/enums/AlarmType.java
@@ -0,0 +1,37 @@
+package likelion.univ.alarm.controller.enums;
+
+import likelion.univ.recruit.usecase.EmailRecruitAlarmUsecase;
+import likelion.univ.recruit.usecase.RecruitAlarmUsecase;
+import likelion.univ.recruit.usecase.SmsRecruitAlarmUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+@RequiredArgsConstructor
+public enum AlarmType {
+    EMAIL(() -> {
+        WebApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
+        return context.getBean("emailRecruitAlarmUsecase", EmailRecruitAlarmUsecase.class);
+    }),
+    SMS(() -> {
+        WebApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
+        return context.getBean("smsRecruitAlarmUsecase", SmsRecruitAlarmUsecase.class);
+    });
+
+    private final Supplier<RecruitAlarmUsecase> sender;
+
+    public static RecruitAlarmUsecase of(String type) {
+        if (Arrays.stream(values()).count() == 0 ) {
+            throw new IllegalStateException("일치하는 타입이 없음");
+        }
+        return valueOf(type.toUpperCase()).supplyUseCases();
+    }
+
+    private RecruitAlarmUsecase supplyUseCases() {
+        return sender.get();
+    }
+
+}
