3114d1120c897b80c5aa5cbf956539f9a8117c66
seulgi99 <tmfrk0426@gmail.com>
2023-09-29 15:46:40 +0900

    fix : yml 설정 날라간거 추가

--- likelion-security/src/main/java/likelion/univ/security/CorsConfig.java
+++ likelion-security/src/main/java/likelion/univ/security/CorsConfig.java
@@ -1,6 +1,5 @@
 package likelion.univ.security;
 
-import likelion.univ.annotation.Processor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
