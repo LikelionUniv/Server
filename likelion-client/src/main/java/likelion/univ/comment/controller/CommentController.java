bd7cfcd8568109e496fd5e8e643b94107f5f8d94
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-10-08 13:21:41 +0900

    fix : commentcontroller uri

--- likelion-client/src/main/java/likelion/univ/comment/controller/CommentController.java
+++ likelion-client/src/main/java/likelion/univ/comment/controller/CommentController.java
@@ -29,31 +29,31 @@ public class CommentController {
     private final GetAllCommentUseCase getAllCommentUseCase;
 
     @Operation(summary = "댓글 작성", description = "부모 댓글을 생성합니다.")
-    @PostMapping("/new/parent")
+    @PostMapping("/parent")
     public SuccessResponse<?> createParentComment(@RequestBody CommentCreateParentRequestDto request) {
         return createParentCommentUseCase.execute(request);
     }
 
     @Operation(summary = "대댓글 작성", description = "자식 댓글을 생성합니다.")
-    @PostMapping("/new/child")
+    @PostMapping("/child")
     public SuccessResponse<?> createChildComment(@RequestBody CommentCreateChildRequestDto request) {
         return createChildCommentUseCase.execute(request);
     }
 
     @Operation(summary = "댓글 내용 수정", description = "댓글 body필드를 수정합니다.")
-    @PatchMapping("/update/{commentId}")
+    @PatchMapping("/{commentId}")
     public SuccessResponse<?> updateComment(@PathVariable Long commentId, @RequestBody CommentUpdateRequestDto request) {
         return updateCommentUseCase.execute(commentId, request);
     }
 
     @Operation(summary = "댓글 삭제", description = "댓글을 soft delete 합니다.")
-    @PatchMapping("/delete/soft/{commentId}")
+    @PatchMapping("/disable/{commentId}")
     public SuccessResponse<?> deleteCommentSoft(@PathVariable Long commentId) {
         return softDeleteCommentUseCase.execute(commentId);// soft delete
     }
 
     @Operation(summary = "댓글 완전 삭제", description = "댓글을 hard delete 합니다.")
-    @DeleteMapping("/delete/hard/{commentId}")
+    @DeleteMapping("/{commentId}")
     public SuccessResponse<?> deleteCommentHard(@PathVariable Long commentId) {
         return hardDeleteCommentUseCase.execute(commentId);
     }
