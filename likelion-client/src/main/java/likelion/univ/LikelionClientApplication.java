a29334262d25b705ab0d3cb3b027ee0607ba844f
Gahyun-Lee <98022068+Gahyun-Lee@users.noreply.github.com>
2023-11-26 13:39:43 +0900

    Update LikelionClientApplication.java

--- likelion-client/src/main/java/likelion/univ/LikelionClientApplication.java
+++ likelion-client/src/main/java/likelion/univ/LikelionClientApplication.java
@@ -3,8 +3,14 @@ package likelion.univ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
+@EnableJpaAuditing
+@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class LikelionClientApplication {
 
