7d1d909050068e520564146ce96599eedf6b0ece
Gahyun-Lee <98022068+Gahyun-Lee@users.noreply.github.com>
2023-12-23 22:45:35 +0900

    fix: 기술 스택 수정 오류 수정

--- likelion-core/src/main/java/likelion/univ/domain/project/repository/TechRepository.java
+++ likelion-core/src/main/java/likelion/univ/domain/project/repository/TechRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TechRepository extends JpaRepository<Tech, Long> {
-    List<Tech> findTechByTechName(String techName);
+    Tech findTechByTechName(String techName);
 }
