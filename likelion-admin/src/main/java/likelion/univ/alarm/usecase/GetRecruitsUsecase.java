0515ed1b9e7eb520d7eeab6557dcbe3ce0162b69
seulgi99 <tmfrk0426@gmail.com>
2024-01-24 00:00:33 +0900

    fix : 대학어드민 리쿠르팅 알람 조회 수정

--- likelion-admin/src/main/java/likelion/univ/alarm/usecase/GetRecruitsUsecase.java
+++ likelion-admin/src/main/java/likelion/univ/alarm/usecase/GetRecruitsUsecase.java
@@ -4,8 +4,11 @@ import likelion.univ.alarm.dto.GetRecruitsDto;
 import likelion.univ.annotation.UseCase;
 import likelion.univ.domain.recruit.entity.Recruit;
 import likelion.univ.domain.recruit.service.RecruitQueryService;
+import likelion.univ.domain.university.adaptor.UniversityAdaptor;
 import likelion.univ.domain.university.entity.University;
+import likelion.univ.domain.user.adaptor.UserAdaptor;
 import likelion.univ.domain.user.entity.User;
+import likelion.univ.utils.AuthenticatedUserUtils;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -15,9 +18,12 @@ import java.util.List;
 public class GetRecruitsUsecase {
 
     private final RecruitQueryService recruitQueryService;
+    private final AuthenticatedUserUtils authenticatedUserUtils;
+    private final UserAdaptor userAdaptor;
+    public GetRecruitsDto execute(int generation) {
+        Long userId = authenticatedUserUtils.getCurrentUserId();
+        University university = userAdaptor.findByIdWithUniversity(userId).getUniversityInfo().getUniversity();
 
-    public GetRecruitsDto execute(User user, int generation) {
-        University university = user.getUniversityInfo().getUniversity();
         List<Recruit> recruits = recruitQueryService
                 .queryAllByUniversityNameAndGeneration(university.getName(), generation);
 
