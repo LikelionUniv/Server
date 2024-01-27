7ce575f01a602e8511fc629cc155202757212238
seulgi99 <tmfrk0426@gmail.com>
2024-01-27 12:26:44 +0900

    fix : 리프레쉬토큰 만료 에러수정

--- likelion-client/src/main/java/likelion/univ/auth/usecase/RefreshTokenUseCase.java
+++ likelion-client/src/main/java/likelion/univ/auth/usecase/RefreshTokenUseCase.java
@@ -5,9 +5,11 @@ import likelion.univ.auth.dto.response.AccountTokenDto;
 import likelion.univ.auth.processor.GenerateAccountTokenProcessor;
 import likelion.univ.domain.user.adaptor.UserAdaptor;
 import likelion.univ.domain.user.entity.User;
+import likelion.univ.exception.ExpiredTokenException;
 import likelion.univ.exception.InvalidTokenException;
 import likelion.univ.jwt.JwtProvider;
 import likelion.univ.jwt.dto.DecodedJwtToken;
+import likelion.univ.refreshtoken.exception.ExpiredRefreshTokenException;
 import likelion.univ.refreshtoken.service.RefreshTokenRedisService;
 import lombok.RequiredArgsConstructor;
 
@@ -27,5 +29,14 @@ public class RefreshTokenUseCase {
         if(refreshTokenRedisService.checkToken(user.getId(), refreshToken)){
             return generateAccountTokenProcessor.refreshToken(user,refreshToken);
         }else throw new InvalidTokenException();
+    }
+
+    private DecodedJwtToken decodeRefreshToken(String refreshToken){
+        try {
+            return jwtProvider.decodeToken(refreshToken,REFRESH_TOKEN);
+        }
+        catch (ExpiredTokenException e) {
+            throw new ExpiredRefreshTokenException();
+        }
     }
 }
