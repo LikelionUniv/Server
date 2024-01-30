780b411978eca13b10ea92ea18e0e64f4c97ee8d
wwan13 <wwan13@naver.com>
2024-01-30 19:01:08 +0900

    fix : 요구사항 수정

--- likelion-core/src/main/java/likelion/univ/domain/alarm/repository/AlarmRepository.java
+++ likelion-core/src/main/java/likelion/univ/domain/alarm/repository/AlarmRepository.java
@@ -3,6 +3,9 @@ package likelion.univ.domain.alarm.repository;
 import likelion.univ.domain.alarm.entity.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     boolean existsByOrdinalAndEmail(Long ordinal, String email);
+    List<Alarm> findAllByOrdinal(Long ordinal);
 }
