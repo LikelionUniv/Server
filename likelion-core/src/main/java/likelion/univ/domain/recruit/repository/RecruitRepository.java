2046a2b21a3506dd737c8ca495336b94cadc8578
shin-mallang <huipulci1@naver.com>
2024-04-08 15:57:08 +0900

    refactor: RecruitAdaptor 제거

--- likelion-core/src/main/java/likelion/univ/domain/recruit/repository/RecruitRepository.java
+++ likelion-core/src/main/java/likelion/univ/domain/recruit/repository/RecruitRepository.java
@@ -2,13 +2,18 @@ package likelion.univ.domain.recruit.repository;
 
 import java.util.List;
 import likelion.univ.domain.recruit.entity.Recruit;
+import likelion.univ.domain.recruit.exception.RecruitNotFound;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
 
+    default Recruit getById(Long id) {
+        return findById(id).orElseThrow(RecruitNotFound::new);
+    }
+
     List<Recruit> findAllByGenerationAndUniversityName(Integer generation, String universityName);
 
-    Boolean existsByEmailAndGeneration(String email, Integer generation);
+    boolean existsByEmailAndGeneration(String email, Integer generation);
 }
--- likelion-core/src/main/java/likelion/univ/domain/recruit/service/RecruitService.java
+++ likelion-core/src/main/java/likelion/univ/domain/recruit/service/RecruitService.java
@@ -1,6 +1,5 @@
 package likelion.univ.domain.recruit.service;
 
-import likelion.univ.domain.recruit.adaptor.RecruitAdaptor;
 import likelion.univ.domain.recruit.entity.Recruit;
 import likelion.univ.domain.recruit.exception.EmailAlreadyRegistered;
 import likelion.univ.domain.recruit.repository.RecruitRepository;
@@ -13,14 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RecruitService {
 
-    private final RecruitAdaptor recruitAdaptor;
     private final RecruitRepository recruitRepository;
 
     public Long register(Recruit recruit) {
         if (recruitRepository.existsByEmailAndGeneration(recruit.getEmail(), recruit.getGeneration())) {
             throw new EmailAlreadyRegistered();
         }
-        return recruitAdaptor.save(recruit);
+        return recruitRepository.save(recruit).getId();
     }
 
     public void delete(long recruitId) {
