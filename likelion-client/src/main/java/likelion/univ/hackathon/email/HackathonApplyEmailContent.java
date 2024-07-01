4a2475679b89dd3c7ab841d46a2568a2c62d88ce
Donghun Shin <huipulci1@naver.com>
2024-07-01 19:06:46 +0900

    feat: 해커톤 신청 시 이메일 전송 (#138)

---
+++ likelion-client/src/main/java/likelion/univ/hackathon/email/HackathonApplyEmailContent.java
@@ -0,0 +1,51 @@
+package likelion.univ.hackathon.email;
+
+public final class HackathonApplyEmailContent {
+
+    public static String VALUE = """
+            <!DOCTYPE html>
+            <html lang="en"><head>
+            		<meta charset="UTF-8">
+            		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+            		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+            		<title>Document</title>
+            	</head>
+            	<body style="overscroll-behavior-x: auto;">
+            		<table align="center" style="width: 596px; table-layout: fixed; max-width: 596px" cellspacing="0" cellpadding="0">
+            			<tbody>
+            				<tr>
+            					<td style="width: 20px"></td>
+            					<td>
+            						<img src="img.png" width="300" height="100" alt="" style="max-width: 100%; height: auto; vertical-align: top">
+            					</td>
+            				</tr>
+            				<tr>
+            					<td style="width: 20px"></td>
+                        
+            					<td style="
+            							padding: 20px 0;
+            							border-radius: 7px;
+            							font-weight: 500;
+            							font-size: 20px;
+            							color: #ffffff;
+            							text-align: center;
+            							background-color: #ff7710;
+            						">
+            						<a href="https://likelion.university/" style="
+            								padding: 20px 62px;
+            								border-radius: 7px;
+            								text-decoration: none;
+            								color: #ffffff;
+            							">
+            							홈페이지로 이동하기
+            						</a>
+            					</td>
+            					<td style="width: 20px"></td>
+            				</tr>
+            			</tbody>
+            		</table>
+            	
+                        
+            </body></html>
+            """;
+}
